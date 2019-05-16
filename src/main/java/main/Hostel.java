71503102451b5ad726d3c71a48cc4214eccc90e0
user <user@DESKTOP-NBOIAGP>
2019-05-16 09:30:12 +0530

    h

--- src/main/java/main/Hostel.java
+++ src/main/java/main/Hostel.java
@@ -41,17 +41,27 @@ public class Hostel{
 		               String checkName = splited[0];
 		               //write your code here !!!
 //		               compare check name with name and return true if present and false if not
+	               if(splited[0].equals(name))
+	               {
+	            	   return true;
+	               }
 	               }
 	               
 	               
 	            }catch(Exception e){
 	                System.out.println(e);
 	            }
-			return true;
+			return false;
 	   }
        public static void allotHostel(){
     	   //write your code here!!!
-    	   
+    	   try {
+    		   writedata();
+    	   }
+    	   catch(IOException e)
+    	   {
+    		   System.out.println(e);
+    	   }
        }
 
        public static boolean verifyStudent(int regNo){
@@ -74,7 +84,7 @@ public class Hostel{
            
        public static boolean verifyName(String name){
     	   boolean chk = true;
-    	   
+    	   chk = readData(name);
     	   //write your code here
     	   
     	   return chk;
